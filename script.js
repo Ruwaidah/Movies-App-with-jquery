f41639abaf2e685f4be178a0a63560e9ed260021
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-06-08 19:28:29 -0500

    styling the page

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ $("#movie-form").on("submit", function (event) {
   event.preventDefault();
   const movie = $("#title").val();
   const rate = $("#rate").val();
+  if (movie != "" || rate !="" )
   $("ul").append(
-    `<li><p>${movie}<p/><p>${rate}</p><button>Remove</button></li>`
+    `<li><p>${movie}</p><p>${rate}</p><button>Remove</button></li>`
   );
 });
 
